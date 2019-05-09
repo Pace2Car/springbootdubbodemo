1ff474ce5fa9246dc1f2354dc620c83fb17fd01e
Pace2Car <Pace2Car@163.com>
2019-05-09 18:40:38 +0800

    Initial commit

---
+++ provider/src/main/java/com/pace2car/dubbodemo/provider/ProviderApplication.java
@@ -0,0 +1,13 @@
+package com.pace2car.dubbodemo.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+}
